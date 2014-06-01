a9729303fe2728870ca00467ff597b9c6286f9e6
Jin <zmfltbsk2@naver.com>
2014-06-01 09:02:51 +0900

    Test 추가

--- MarchingCubes/Assets/Voxel.cs
+++ MarchingCubes/Assets/Voxel.cs
@@ -18,6 +18,8 @@ public class Voxel : MonoBehaviour
     MarchingCubes generator;
     float[, ,] voxelData;
 
+	Mesh mesh;
+
     void Start()
     {
         generator = new MarchingCubes(0.0f);
@@ -41,6 +43,7 @@ public class Voxel : MonoBehaviour
         Mesh cubeMesh = generator.CreateMesh(voxelData, new Vector3((float)width / 2.0f, (float)height / 2.0f, (float)length / 2.0f));
         cubeMesh.uv = new Vector2[cubeMesh.vertices.Length];
         cubeMesh.RecalculateNormals();
+		mesh = cubeMesh;
 
         GameObject child = new GameObject("0");
         child.AddComponent<MeshFilter>();
@@ -66,62 +69,160 @@ public class Voxel : MonoBehaviour
     public Object cubeObj;
     public GameObject temp;
 
-    void Update()
-    {
-        if (Input.GetMouseButtonDown(1))
-        {
-            foreach (Transform child in temp.transform)
-            {
-                Destroy(child.gameObject);
-            }
-        }
-        if (Input.GetMouseButtonDown(0))
-        {
-            List<OcTree> nodeList = new List<OcTree>();
-            List<float> dist = new List<float>();
-            octree.find(Camera.main.ScreenPointToRay(Input.mousePosition), ref nodeList, ref dist, width, height, length, voxelData);
+	bool PickingTestOne()	
+	{
+		List<OcTree> nodeList = new List<OcTree>();
+		List<float> dist = new List<float>();
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		octree.find(ray, ref nodeList, ref dist, width, height, length, voxelData);
+		
+		int si = 0;
+		float sd = 1000000000;
+		int count = dist.Count;
+		bool find = false;
+		for (int i = 0; i < count; ++i)
+		{
+			if (sd > dist[i])
+			{
+				for (int j = 0; j < 8; ++j)
+				{
+					int x, y, z;
+					OcTree.Calc3rdDimIdx(nodeList[i].corners[j], width, height, length, out x, out y, out z);
+					if (voxelData[x, y, z] < generator.densityTarget)
+					{
+						sd = dist[i];
+						si = i;
+						find = true;
+						break;
+					}
+				}
+			}
+		}
+		
+		if (find == false)
+			return false;
+		
+		for (int i = 0; i < 8; ++i)
+		{
+			int x, y, z;
+			OcTree.Calc3rdDimIdx(nodeList[si].corners[i], width, height, length, out x, out y, out z);
+			
+			List<Vector3> lists = new List<Vector3>();
+			generator.Test(x, y, z, new Vector3((float)width / 2.0f, (float)height / 2.0f, (float)length / 2.0f), voxelData, lists);
+			
+			for(int h=0; h<lists.Count; h+=3)
+			{
+				float u, v, t;
+				if(IntersectTriangle(ray, lists[h+0], lists[h+1], lists[h+2], out u, out v, out t))
+				{
+//					Debug.Log("Check");
+//					Vector3 pos = lists[h] 	+ u * (lists[h+1] - lists[h]) 
+//						+ v * (lists[h+2] - lists[h]); 
+//					
+//					GameObject newObj = Instantiate(cubeObj) as GameObject;
+//					newObj.transform.localPosition = pos;
+//					newObj.transform.parent = temp.transform;
+					
+					return true;
+				}
+			}
+			
+//			for (int h = 0; h < lists.Count; ++h)
+//			{
+//				GameObject newObj = Instantiate(cubeObj) as GameObject;
+//				newObj.transform.localPosition = lists[h];
+//				newObj.transform.parent = temp.transform;
+//			}
+		}
 
-            int si = 0;
-            float sd = 1000000000;
-            int count = dist.Count;
-            bool find = false;
-            for (int i = 0; i < count; ++i)
-            {
-                if (sd > dist[i])
-                {
-                    for (int j = 0; j < 8; ++j)
-                    {
-                        int x, y, z;
-                        OcTree.Calc3rdDimIdx(nodeList[i].corners[j], width, height, length, out x, out y, out z);
-                        if (voxelData[x, y, z] < generator.densityTarget)
-                        {
-                            sd = dist[i];
-                            si = i;
-                            find = true;
-                            break;
-                        }
-                    }
-                }
-            }
+		return false;
+	}
 
-            if (find == false)
-                return;
+	bool PickingTestTwo()
+	{
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
-            for (int i = 0; i < 8; ++i)
-            {
-                int x, y, z;
-                OcTree.Calc3rdDimIdx(nodeList[si].corners[i], width, height, length, out x, out y, out z);
+		int[] indices = mesh.GetIndices (0);
+		Vector3[] vertices = mesh.vertices;
 
-                List<Vector3> lists = new List<Vector3>();
-                generator.Test(x, y, z, new Vector3((float)width / 2.0f, (float)height / 2.0f, (float)length / 2.0f), voxelData, lists);
+		int size = indices.Length;	
+		for (int i=0; i<size; i+=3)
+		{
+			Vector3 p0 = vertices[ indices[i] ];
+			Vector3 p1 = vertices[ indices[i+1] ];
+			Vector3 p2 = vertices[ indices[i+2] ];
 
-                for (int h = 0; h < lists.Count; ++h)
-                {
-                    GameObject newObj = Instantiate(cubeObj) as GameObject;
-                    newObj.transform.localPosition = lists[h];
-                    newObj.transform.parent = temp.transform;
-                }
-            }
-        }
-    }
+			float u,v,t;
+			if( IntersectTriangle(ray, p0, p1, p2, out u, out v, out t) )
+			{
+				return true;
+			}
+		}
+
+		return false;
+	}
+	
+	void Update()
+	{
+		float beforeTime = Time.time;
+
+		if (Input.GetMouseButtonDown(1))
+		{
+			for(int i=0; i<1000; ++i)
+				PickingTestTwo();
+
+			Debug.Log(Time.time - beforeTime);
+		}
+		if (Input.GetMouseButtonDown(0))
+		{
+			for(int i=0; i<1000; ++i)
+				PickingTestOne();
+
+			Debug.Log(Time.time - beforeTime);
+		}
+	}
+	
+	bool IntersectTriangle(Ray ray, Vector3 v0, Vector3 v1, Vector3 v2, out float u, out float v, out float t)
+	{
+		u = v = t = -1;
+		
+		Vector3 edge1 = v1 - v0;
+		Vector3 edge2 = v2 - v0;
+		
+		Vector3 pvec = Vector3.Cross (ray.direction, edge2);
+		
+		float determinant = Vector3.Dot (edge1, edge2);
+		
+		Vector3 tvec;
+		if( determinant > 0.0f )
+		{
+			tvec = ray.origin - v0;
+		}
+		else
+		{
+			tvec = v0 - ray.origin;
+			determinant = -determinant;
+		}
+
+		if (determinant < float.Epsilon)
+			return false;
+
+		u = Vector3.Dot (tvec, pvec);
+		if (u < 0.0f || u > determinant)
+			return false;
+
+		Vector3 qvec = Vector3.Cross (tvec, edge1);
+
+		v = Vector3.Dot (ray.direction, qvec);
+		if (v < 0.0f || (u + v) > determinant)
+			return false;
+
+		t = Vector3.Dot (edge2, qvec);
+		float invDet = 1.0f / determinant;
+		t *= invDet;
+		u *= invDet;
+		v *= invDet;
+
+		return true;
+	}
 }
